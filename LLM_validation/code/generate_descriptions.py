14bfc454eeecf67181273dd514f5b290914022fd
Keith Burghardt <keithab@isi.edu>
2024-05-27 20:27:25 -0700

    Update generate_descriptions.py

--- LLM_validation/code/generate_descriptions.py
+++ LLM_validation/code/generate_descriptions.py
@@ -10,10 +10,11 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 from openai import OpenAI
 
 
+
 openai_organization = ""
 openai_api_key = ""
 #model_engine = "gpt-4-turbo-preview"#
-model_engine = "gpt-3.5-turbo"
+model_engine = "gpt-4o"
 client = OpenAI(
     # This is the default and can be omitted
     api_key=openai_api_key,
@@ -44,12 +45,16 @@ def clean_ai_tweets(tweets):
     return new_tweets
 # data from: https://esoc.princeton.edu/publications/trends-online-influence-efforts
 io_files = ['Princeton_InfoOps/IE_database_2023FEB.csv']
+print(io_files)
+ioaccounts = pd.read_csv('CoordResults/gpt-4o_IO_GT.csv')
 ioaccounts = pd.concat([pd.read_csv(file) for file in io_files]).reset_index()
+ioaccounts = ioaccounts.replace({'Suppport':'Support'})
 io_cols = ['targeted country', 'attacking country', 'Political goal category','political goal ', 'event description', 'starting month']
 legible_cols = ['Country Targeted By Information Operation', 'Country Running Information Operation', 'Type of Political Goal','Political Goal of Information Operation', 'Description of Information Operation', 'Starting Month of Information Operation']
 ioaccounts= ioaccounts[io_cols].drop_duplicates()
 example_tweets = []
 for ii,row in ioaccounts.iterrows():
+    print(ii)
     information = {c_leg:row[c] for c,c_leg in zip(io_cols,legible_cols)}
     prompt = 'An information operation is dissemination of propaganda in pursuit of a competitive advantage over an opponent. Here is a description of an information operation: '+str(information)
     prompt +=' \n Create 10 tweets (aka X posts) in the style of this information operation. Make your output in the form of a Python array.'
@@ -60,17 +65,25 @@ for ii,row in ioaccounts.iterrows():
 ioaccounts['tweets'] = example_tweets
 # test if we can find info from these tweets
 ioaccounts['new_tweets'] = clean_ai_tweets(ioaccounts['tweets'].values)
+ioaccounts.to_csv(model_engine+'_IO_GT.csv',index=False)    
 attempted_answer = {c:[] for c in io_cols}
+no_cot = True
 for ii,row in ioaccounts.iterrows():
+    print(ii)
     tweets = row['new_tweets']
-    
     for c,c_leg in zip(io_cols,legible_cols):
         prompt = 'An information operation is dissemination of propaganda in pursuit of a competitive advantage over an opponent. Here are posts from an information operation in the form of a Python array:\n'+str(tweets)
         prompt += '\n Answer the following question about these posts with as few words as possible:\n'
-        prompt += 'State the '+c_leg+'\n'
+        prompt += 'State the '+c_leg
+        
+        if c == 'Political goal category':
+            prompt += ' where the categories are '+', '.join(ioaccounts['Political goal category'].dropna().drop_duplicates().values.tolist())
+        if no_cot:
+            prompt+='. Write your answer in one sentence.\n'
+        else:
+            prompt+='. Write your answer in one sentence. Create a new paragraph and explain each step when reaching your decision.\n'
         completion = client.chat.completions.create(model=model_engine,messages=[{"role": "user", "content": prompt}])
         response = completion.choices[0].message.content
         attempted_answer[c].append(response)
 
-pd.DataFrame(attempted_answer).to_csv(model_engine+'_Answers.csv',index=False)
-ioaccounts.to_csv(model_engine+'_IO_GT.csv',index=False)    
+pd.DataFrame(attempted_answer).to_csv(model_engine+'_Answers_GPT4oGT_NoCOT='+str(no_cot)+'.csv',index=False)
